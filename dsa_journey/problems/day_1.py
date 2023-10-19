14c1fc30ad77641167966fd34fbe49e7e75323c7
sumit-coder <sumitbu24@gmail.com>
2023-10-19 22:07:57 +0530

    day one problem

---
+++ dsa_journey/problems/day_1.py
@@ -0,0 +1,14 @@
+
+
+def gcdOfStrings(self, str1: str, str2: str) -> str:
+    def isDevisor(l):
+        if len(str1) % l or len(str2) % l:
+            return False
+
+        return str1[:l] * (len(str1) // l) == str1 and str1[:l] * (len(str2) // l) == str2
+
+    for i in range(min(len(str1), len(str2)), 0, -1):
+        if isDevisor(i):
+            return str1[:i]
+
+    return ""
