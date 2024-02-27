1eed15b660b87826609ab77dabbf5c5e99285c3e
sumit-coder <sumitbu24@gmail.com>
2024-02-27 21:43:52 +0530

    learn more graph topic

--- dsa_journey/graph/basic_graph.py
+++ dsa_journey/graph/basic_graph.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 graph = {
     "a": ["b", "c"],
     "b": ["d"],
@@ -7,7 +10,17 @@ graph = {
     "f": [],
 }
 
+graphTwo = [
+    ["b", "c"],
+    ["d", "a"],
+    ["e", "a"],
+    ["f", "c"],
+    ["a", "c"],
+]
+
 # DFS With Stack
+
+
 def graphWithStack(graph, node):
     stack = [node]
     while len(stack) > 0:
@@ -17,14 +30,31 @@ def graphWithStack(graph, node):
             stack.append(n)
 
 # DFS With Recursion
+
+
 def dfsOnGraph(graph, node):
     print(node)
     for n in graph[node]:
         dfsOnGraph(graph, n)
 
 # BFS With Queue
+
+
 def bfsWithQueue(graph, node):
     queue = []
 
-graphWithStack(graph, "a")
-        
+
+def graphToAdjList(undirectedGraph):
+    newGraph = defaultdict(list)
+
+    for nodeGroup in undirectedGraph:
+        node1 = nodeGroup[0]
+        node2 = nodeGroup[1]
+
+        newGraph[node1].append(node2)
+        newGraph[node2].append(node1)
+
+    print(newGraph)
+
+
+graphToAdjList(graphTwo)
